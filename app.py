29bf4d455eef0430c68237cca4625d8cb1df73f7
damartin95 <104377564+damartin95@users.noreply.github.com>
2022-10-03 20:08:41 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+from flask.wrappers import Response
+import git
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def index:()
+  return render_template("index.html")
+

6914e05d934d3c2477659574c79c61d5d71c1b84
damartin95 <104377564+damartin95@users.noreply.github.com>
2022-10-10 19:34:39 -0400

    git-update added

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ import git
 
 app = Flask(__name__)
 
-
-
+@app.route('/git-update', methods=['POST])
+def git_update():
+  repo = git.Repo('./pyanywhere')
+  origin = repo.remotes.origin
+  repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()                                
+  origin.pull()
+  return '', 200
 
 @app.route('/')
 def index():

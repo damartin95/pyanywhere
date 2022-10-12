2c3e5fb0b17ec4749b9211e268bc840ccd04bff2
damartin95 <104377564+damartin95@users.noreply.github.com>
2022-10-11 20:24:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,10 @@ def git_update():
 def index():
   return render_template("json.html")
 
-#background process happening without any refreshing
-@app.route('/background_process_test')
-def background_process_test():
-    print ("Hello1")
-    return ("test")
+@app.route('/square', methods=['POST']) 
+def square(): 
+	num = float(request.form.get('number', 0)) 
+	square = num ** 2 
+	data = {'square': square} 
+	data = jsonify(data) 
+	return data 

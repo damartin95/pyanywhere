cc7d8dc8fa49e10db344eafbc39176e57b98fd85
damartin95 <104377564+damartin95@users.noreply.github.com>
2022-10-11 22:00:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,3 +15,15 @@ def git_update():
 @app.route('/')
 def index():
   return render_template("index.html")
+
+
+@app.route('/square/', methods=['POST']) 
+def square(): 
+	num = float(request.form.get('number', 0)) 
+	square = num ** 2 
+	data = {'square': square} 
+	data = jsonify(data) 
+	return data 
+ 
+if __name__ == '__main__': 
+	app.run(debug=True)
